b28f1a77654734d4a758a397b5b0db05e99a3f2b
vengeannnce <a.dudaevv@yandex.ru>
2026-02-27 22:19:26 +0300

    Дополнение под задание 1.4

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from models import User
 
 app = FastAPI()
 
+# Задание 1.2
 @app.get("/")
 async def read_index():
     return FileResponse("index.html")
+
+# Задание 1.4
+@app.get("/users")
+async def get_user():
+    user = User(name="Азамат Дудаев", id=5985)
+    return user
